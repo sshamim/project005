ffbce41bde5d76aa2a2960407896e35f7dd9a2f2
Saqib Shamim <saqibshamim@yahoo.com>
2020-06-17 21:38:53 +0500

    Updating Value
    Updated App.js and Child.js to demo the step of updating the values

--- src/Child.js
+++ src/Child.js
@@ -2,12 +2,15 @@ import React, { useContext } from 'react';
 import ValueContext from './ValueContext';
 
 function Child() {
-    let value = useContext(ValueContext)
-      return (
-        <div >
-          Child number: {value}
-        </div>
-      );
+    let count = useContext(ValueContext)
+      let updateValue = count[1]
+        return (
+          <div >
+            Child number: {count[0]}
+            <br />
+            <button onClick={()=>{updateValue (++count[0])}}>Increase</button>
+          </div>
+        );
   }
   
   export default Child;
