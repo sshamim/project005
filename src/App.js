6858a10fa8edfa695822f25832d8ee2c7df73372
Saqib Shamim <saqibshamim@yahoo.com>
2020-06-17 21:55:39 +0500

    Resolved Conflicts

--- src/App.js
+++ src/App.js
@@ -3,20 +3,6 @@ import './App.css';
 import Parent from './Parent';
 import ValueContext from './ValueContext';
 
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-function App(props) {
-
-  let [number, setNumber] = useState(1)
-
-  return (
-    <div>
-      Hello World
-      <Parent num = {number}></Parent>
-      <button onClick={() => {setNumber(++number)}}>Update Number</button>
-    </div>
-  );
-=======
 function App() {
     // Providing a value to be passed on to Provider as initial value
     // Since we are using useState, it generally returns two values
@@ -25,29 +11,17 @@ function App() {
     // value[1] = some  function
 
     let number = useState(1)
-       
-    return (
-        // Adding a Wrapper
-        <ValueContext.Provider value={number}>
-=======
-function App() {
-    // Providing a value to be passed on to Provide as initial value
-    let value = 1;
-       
-    return (
-        // Adding a Wrapper
-        <ValueContext.Provider value={value}>
->>>>>>> ffbce41bde5d76aa2a2960407896e35f7dd9a2f2
-            <div>
-              Hello World
-              <Parent></Parent>
-            </div>
-        </ValueContext.Provider>
-    );  
-<<<<<<< HEAD
->>>>>>> Stashed changes
-=======
->>>>>>> ffbce41bde5d76aa2a2960407896e35f7dd9a2f2
+        
+      return (
+          // Adding a Wrapper
+          <ValueContext.Provider value={number}>
+
+              <div>
+                Hello World
+                <Parent></Parent>
+              </div>
+          </ValueContext.Provider>
+      );  
 }
 
 export default App;
--- src/Child.js
+++ src/Child.js
@@ -1,18 +1,7 @@
 import React, { useContext } from 'react';
 import ValueContext from './ValueContext';
 
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-function Child(props) {
-       
-    return (
-      <div >
-        Child number: {props.num}
-      </div>
-    );
-=======
-=======
->>>>>>> ffbce41bde5d76aa2a2960407896e35f7dd9a2f2
+
 function Child() {
     let count = useContext(ValueContext)
       let updateValue = count[1]
@@ -23,10 +12,6 @@ function Child() {
             <button onClick={()=>{updateValue (++count[0])}}>Increase</button>
           </div>
         );
-<<<<<<< HEAD
->>>>>>> Stashed changes
-=======
->>>>>>> ffbce41bde5d76aa2a2960407896e35f7dd9a2f2
   }
   
   export default Child;
